c2309eaf3083dfd8af3672cd971ac755cf0db9f8
lungu01 <71168702+lungu01@users.noreply.github.com>
2021-09-28 14:46:17 +0300

    Add files via upload

---
+++ Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3.Ex_1
+{
+    class Box
+    {
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public float Depth { get; set; }
+
+        public Box()
+        {
+            Height = 1;
+            Width = 1;
+            Depth = 1;
+        }
+
+        public Box(float value)
+        {
+            Height = value;
+            Width = value;
+            Depth = value;
+        }
+
+        public Box(float height, float width, float depth)
+        {
+            Height = height;
+            Width = width;
+            Depth = depth;
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3.Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box Obj1;
+            Box Obj2;
+            Box Obj3;
+        }
+    }
+}

9aad407aca57e58aa85a9a09843271f014de14e8
lungu01 <71168702+lungu01@users.noreply.github.com>
2021-12-04 18:11:41 +0200

    Add files via upload

---
+++ Cub.java
@@ -0,0 +1,16 @@
+public class Cub implements GeometricBody {
+    private float length;
+
+    public Cub(float l) {
+        length = l;
+    }
+
+    public float getSuprafata() {
+        return 6 * (length * length);
+    }
+
+    public float getVolum() {
+        return length * length * length;
+    }
+
+}
---
+++ GeometricBody.java
@@ -0,0 +1,4 @@
+public interface GeometricBody {
+    float getSuprafata();
+    float getVolum();
+}
